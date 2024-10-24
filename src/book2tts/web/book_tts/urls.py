194fa5b29018038c26aeb55c0ac04c63ab509e71
pingfury <pingfury@outlook.com>
2024-10-24 11:19:58 +0800

    add django

---
+++ src/book2tts/web/book_tts/urls.py
@@ -0,0 +1,17 @@
+"""
+URL configuration for book_tts project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("", include("home.urls")),
+    path("admin/", admin.site.urls),
+]
