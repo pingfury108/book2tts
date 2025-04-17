3f19768bab98eaee0fc0037927813f71291c6390
pingfury <pingfury@outlook.com>
2025-04-17 12:57:43 +0800

    fix home ui

--- src/web/home/urls.py
+++ src/web/home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("audio/<int:segment_id>/", views.audio_detail, name="audio_detail"),
+    path("book/<int:book_id>/", views.book_audio_list, name="book_audio_list"),
 ]
--- src/web/home/views.py
+++ src/web/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from workbench.models import AudioSegment
+from workbench.models import AudioSegment, Books
 
 # Create your views here.
 
@@ -34,3 +34,28 @@ def audio_detail(request, segment_id):
     }
     
     return render(request, "home/audio_detail.html", context)
+
+
+def book_audio_list(request, book_id):
+    # 获取书籍对象，确保它存在
+    book = get_object_or_404(Books, id=book_id)
+    
+    # 确保用户只能访问自己的书籍
+    if request.user != book.user:
+        from django.http import Http404
+        raise Http404("您没有权限访问此页面")
+    
+    # 获取该书籍下已发布的音频片段
+    book_audio_segments = AudioSegment.objects.filter(
+        book=book,
+        user=request.user,
+        published=True
+    ).order_by('-id')  # 按ID降序排列（最新的在前）
+    
+    context = {
+        'book': book,
+        'audio_segments': book_audio_segments,
+        'display_title': f'《{book.name}》的音频列表'
+    }
+    
+    return render(request, "home/index.html", context)
