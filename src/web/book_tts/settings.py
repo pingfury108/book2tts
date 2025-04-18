3bc4843da21832f3121e4547ff4e9c541d42c7dc
pingfury <pingfury@outlook.com>
2025-04-18 23:51:44 +0800

    fix settings

--- src/web/book_tts/settings.py
+++ src/web/book_tts/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +27,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-v6hwk0!^h(!#ri9&86uhzf))s-u!8e1@4ta^(ixa=+0%4u!qp*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip() for origin in os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+    if origin.strip() and (origin.strip().startswith("http://") or origin.strip().startswith("https://"))
+]
+
 
 # Application definition
 
